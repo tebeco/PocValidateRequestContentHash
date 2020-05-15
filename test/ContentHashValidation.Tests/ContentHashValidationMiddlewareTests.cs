922208869a575ba71c7f0e2485f059557b419c7d
TeBeCo <tebeco@gmail.com>
2020-05-15 18:48:31 +0200

    unit tests have to call SetEndpoint to set the Metadata to trigger the middleware

--- test/ContentHashValidation.Tests/ContentHashValidationMiddlewareTests.cs
+++ test/ContentHashValidation.Tests/ContentHashValidationMiddlewareTests.cs
@@ -27,9 +27,44 @@ namespace ContentHashValidation.Tests
             httpContext.Request.Body = new MemoryStream(buffer); //16Ko
             httpContext.Request.Headers[ContentHashValidationOptions.DefaultHeaderName] = hashStringified;
             httpContext.Request.Method = "POST";
-            httpContext.Request.Path = isValidatedRoute ? "/validationcontent/validated" : "/";
+
+            if (isValidatedRoute)
+            {
+                httpContext.SetEndpoint(new Endpoint(c => Task.CompletedTask,
+                                                     new EndpointMetadataCollection(new ValidateContentHashAttribute()),
+                                                     "someRoute"));
+            }
 
             var next = new RequestDelegate(context => { context.Response.StatusCode = 200; return Task.CompletedTask; });
+            var middleware = new ContentHashValidationMiddleware(next, Options.Create(new ContentHashValidationOptions()));
+
+            await middleware.InvokeAsync(httpContext);
+
+            Assert.Equal(200, httpContext.Response.StatusCode);
+        }
+
+        [Theory]
+        [InlineData(200, 10)]
+        [InlineData(302, 10)]
+        [InlineData(400, 10)]
+        [InlineData(404, 10)]
+        [InlineData(500, 10)]
+        [InlineData(503, 10)]
+        public async Task Should_not_change_existing_StatusCode_When_hash_match(int statusCode, int payloadSize)
+        {
+            var buffer = Encoding.UTF8.GetBytes(new string('-', payloadSize));
+            var hash = SHA256.Create().ComputeHash(buffer);
+            var hashStringified = BitConverter.ToString(hash).Replace("-", "");
+
+            var httpContext = new DefaultHttpContext();
+            httpContext.Request.Body = new MemoryStream(buffer); //16Ko
+            httpContext.Request.Headers[ContentHashValidationOptions.DefaultHeaderName] = hashStringified;
+            httpContext.Request.Method = "POST";
+            httpContext.SetEndpoint(new Endpoint(c => Task.CompletedTask,
+                                                 new EndpointMetadataCollection(new ValidateContentHashAttribute()),
+                                                 "someRoute"));
+
+            var next = new RequestDelegate(_ => Task.CompletedTask);
             var middleware = new ContentHashValidationMiddleware(next, Options.Create(new ContentHashValidationOptions()));
 
             await middleware.InvokeAsync(httpContext);
@@ -50,7 +85,9 @@ namespace ContentHashValidation.Tests
             httpContext.Request.Body = new MemoryStream(buffer); //16Ko
             httpContext.Request.Headers[ContentHashValidationOptions.DefaultHeaderName] = new string('0', 64);
             httpContext.Request.Method = "POST";
-            httpContext.Request.Path = "/validationcontent/validated";
+            httpContext.SetEndpoint(new Endpoint(c => Task.CompletedTask,
+                                                 new EndpointMetadataCollection(new ValidateContentHashAttribute()),
+                                                 "someRoute"));
 
             var next = new RequestDelegate(_ => Task.CompletedTask);
             var middleware = new ContentHashValidationMiddleware(next, Options.Create(new ContentHashValidationOptions()));
