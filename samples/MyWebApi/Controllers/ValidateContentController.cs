9bd3ac07f011e1636308e8334dd30615392ad74a
TeBeCo <tebeco@gmail.com>
2020-05-19 08:43:24 +0200

    Add InOutLoggingMetadata for endpoint and update the Controller for show case

--- samples/MyWebApi/Controllers/ValidateContentController.cs
+++ samples/MyWebApi/Controllers/ValidateContentController.cs
@@ -1,21 +1,38 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using ContentHashValidation;
+using InOutLogging;
 
 namespace MyWebApi.Controllers
 {
     [ApiController]
     [Route("[controller]")]
-    public class ValidateContentController : ControllerBase
+    public class NoBindingController : ControllerBase
     {
-        [HttpPost("validated")]
-        [ValidateContentHash]
-        public ActionResult Validated()
+        [HttpPost("nothing")]
+        public ActionResult Nothing()
         {
             return Ok();
         }
 
-        [HttpGet("not-validated")]
-        public ActionResult NotValidated()
+        [HttpPost("only-hash")]
+        [ValidateContentHash]
+        [NoContentInOutLogging]
+        public ActionResult Only_Hash()
+        {
+            return Ok();
+        }
+
+        [HttpPost("hash-no-content")]
+        [ValidateContentHash]
+        [NoContentInOutLogging]
+        public ActionResult Hash_Without_Content_InOnLogging()
+        {
+            return Ok();
+        }
+
+        [HttpPost("all")]
+        [ValidateContentHash]
+        public ActionResult Hash_With_InOnLogging()
         {
             return Ok();
         }
---
+++ src/InOutLogging/InOutLoggingAttribute.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace InOutLogging
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = false, Inherited = true)]
+    public abstract class InOutLoggingBaseAttribute : Attribute, IInOutLoggingMetadata
+    {
+        public virtual bool IgnoreContent { get; set; } = false;
+
+        public virtual bool IsExcluded { get; set; } = false;
+    }
+
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = false, Inherited = true)]
+    public class NoContentInOutLoggingAttribute : InOutLoggingBaseAttribute, IInOutLoggingMetadata
+    {
+        public override bool IgnoreContent { get; set; } = true;
+    }
+
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = false, Inherited = true)]
+    public class IgnoreInOutLoggingAttribute : InOutLoggingBaseAttribute, IInOutLoggingMetadata
+    {
+        public override bool IsExcluded { get; set; } = true;
+    }
+}
